ec0ce727147d2cd44554898ae8ecbaee0f97ce36
BetterCallSalar <salar.eini.cr@gmail.com>
2024-08-20 10:03:02 +0200

    refactor: Update footer links in footer.php
    - Update the contact link in the footer to use the appropriate controller and action.
    - Add a new link to the about page in the footer.
    
    Note: This commit message follows the convention of starting with a verb in the imperative form and providing a concise summary of the changes made.

--- views/_partial/footer.php
+++ views/_partial/footer.php
@@ -4,8 +4,8 @@
 <footer class="py-5 text-center">
 	<p>Created By Better.Call.Salar </p>
 	<p>
-		<a href="index.php?ctrl=page&action=mentions" class="text-decoration-none">Legal Mentions</a>
-		| <a href="index.php?ctrl=page&action=contact" class="text-decoration-none">Contact</a>
+		<a href="index.php?Controller=page&Action=contact" class="text-decoration-none">Contact</a> |
+		<a href="index.php?Controller=page&Action=about" class="text-decoration-none">About</a>
 	</p>
 	<p class="mb-0">
 		<a href="#" class="text-decoration-none">Back to top</a>
@@ -13,7 +13,6 @@
 </footer>
 </div>
 
-<!-- Bootstrap JS -->
 <script src="assets/js/script.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
 
